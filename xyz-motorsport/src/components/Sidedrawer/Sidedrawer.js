c424a360236eb3814f5ef82682b7c559b3a685ee
Sina Alsharif <sina.alsharif@gmail.com>
2019-12-15 22:39:15 +1100

    Cart is full width on mobile.

--- xyz-motorsport/src/components/Sidedrawer/Sidedrawer.js
+++ xyz-motorsport/src/components/Sidedrawer/Sidedrawer.js
@@ -5,7 +5,7 @@ import {Cart} from "../Cart/Cart";
 class Sidedrawer extends Component {
     render() { 
         return ( 
-            <div id="Sidedrawer" className = "h-full shadow overflow-scroll w-1/3 max-w-64 bg-white fixed z-50 p-4">
+            <div id="Sidedrawer" className = "h-full shadow overflow-scroll bg-white fixed z-50 p-4">
                 <button className="bg-red-500 hover:bg-red-700 text-white p-2 rounded right-0 float-right" onClick = {this.props.drawerClickHandler}>Close</button>
                 <Cart/>
             </div>
